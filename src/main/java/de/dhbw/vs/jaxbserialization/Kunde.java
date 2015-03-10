cc4324c748198266be074555df64a535c2a5aa9d
Prof. Dr. Julian Reichwald <julian.reichwald@dhbw-mannheim.de>
2015-03-10 11:41:21 +0100

    Initial Commit

---
+++ src/main/java/de/dhbw/vs/jaxbserialization/Kunde.java
@@ -0,0 +1,46 @@
+package de.dhbw.vs.jaxbserialization;
+
+import javax.xml.bind.annotation.XmlAttribute;
+import javax.xml.bind.annotation.XmlRootElement;
+
+
+/**
+ * 
+ * @author Prof. Dr. Julian Reichwald <julian.reichwald@dhbw-mannheim.de>
+ * 
+ * Repräsentation eines Kunden
+ */
+
+@XmlRootElement
+public class Kunde {
+
+	private String vorname ; 
+	
+	private String nachname; 
+	
+	private int kundennummer;
+
+	public String getVorname() {
+		return vorname;
+	}
+
+	public void setVorname(String vorname) {
+		this.vorname = vorname;
+	}
+
+	public String getNachname() {
+		return nachname;
+	}
+
+	public void setNachname(String nachname) {
+		this.nachname = nachname;
+	}
+
+	public int getKnr() {
+		return kundennummer;
+	}
+	@XmlAttribute(name="Kundennummer")
+	public void setKnr(int alter) {
+		this.kundennummer = alter;
+	} 
+}
---
+++ src/main/java/de/dhbw/vs/jaxbserialization/TestMain.java
@@ -0,0 +1,49 @@
+package de.dhbw.vs.jaxbserialization;
+
+import java.io.File;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBElement;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
+import javax.xml.bind.Unmarshaller;
+import javax.xml.bind.Validator;
+import javax.xml.namespace.QName;
+
+/**
+ * Testprogramm für JAXB-Marshalling 
+ * @author Prof. Dr. Julian Reichwald <julian.reichwald@dhbw-mannheim.de>
+ *
+ */
+
+public class TestMain {
+	
+	public static void main(String[] args) {	
+		
+		Kunde p = new Kunde(); 
+		p.setVorname("Max");
+		p.setNachname("Mustermann");
+		p.setKnr(42);
+		
+		try {
+			// JAXB initialisieren
+			JAXBContext kundenContext = JAXBContext.newInstance(Kunde.class);
+			Marshaller jaxbMarshaller = kundenContext.createMarshaller();
+			Unmarshaller jaxbUnmarshaller = kundenContext.createUnmarshaller();
+			jaxbMarshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
+			
+			// Datei erzeugen 
+			File f = File.createTempFile("jaxbexample",".xml");
+			
+			// Kunde in tmp-Datei schreiben
+			jaxbMarshaller.marshal(p, f);			
+			
+			System.out.println("XML geschrieben in Datei: " + f.toString());
+			
+			// Kunde aus tmp-Datei lesen
+			Kunde kundeAusXML = (Kunde)jaxbUnmarshaller.unmarshal(f);
+			System.out.println("Kunde in XML-Datei: " + kundeAusXML.getKnr()+", "+kundeAusXML.getVorname()+" "+kundeAusXML.getNachname());
+			
+		} catch (Exception e) { e.printStackTrace(); }
+	}
+}
